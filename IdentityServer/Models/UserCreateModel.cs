ae876e3fb1d7bf03774b4315a49d4fc2d6c9a08f
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-10-15 20:11:32 +0300

    User create view completed & post create method is wrote

--- IdentityServer/Models/UserCreateModel.cs
+++ IdentityServer/Models/UserCreateModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,16 @@ namespace IdentityServer.Models
 {
     public class UserCreateModel
     {        
+        [Required(ErrorMessage ="Username is required")]
         public string Username { get; set; }
+        [EmailAddress(ErrorMessage ="You have to enter type of Email")]
+        [Required(ErrorMessage ="Email is required")]
         public string Email { get; set; }
+        [Required(ErrorMessage ="Password is required")]
         public string Password { get; set; }
+        [Compare("Password",ErrorMessage ="Passwords are can not match matched")]
         public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage ="Gender is required")]
         public string Gender { get; set; }
     }
 }
