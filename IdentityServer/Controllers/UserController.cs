2cdbbcfe43a99745bf635eaac9018c7d3b7bccc9
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-10-21 22:03:14 +0300

    I get the users from identityDb

--- IdentityServer/Controllers/UserController.cs
+++ IdentityServer/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IdentityServer.Context;
+using IdentityServer.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,11 +10,41 @@ using System.Threading.Tasks;
 
 namespace IdentityServer.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class UserController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IdentityContext _context;
+        public UserController(UserManager<AppUser> userManager, IdentityContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var query = _userManager.Users;
+            var users = _context.Users.Join(_context.UserRoles, user => user.Id, userRole => userRole.UserId, (user, userRole) => new
+            {
+                user,
+                userRole
+            }).Join(_context.Roles, two => two.userRole.RoleId, role => role.Id, (two, role) => new { two.user, two.userRole, role }).Where(x => x.role.Name != "Admin").Select(x => new AppUser
+            {
+                Id = x.user.Id,
+                AccessFailedCount = x.user.AccessFailedCount,
+                ConcurrencyStamp = x.user.ConcurrencyStamp,
+                Email = x.user.Email,
+                EmailConfirmed = x.user.EmailConfirmed,
+                Gender = x.user.Gender,
+                ImagePath = x.user.ImagePath,
+                LockoutEnabled = x.user.LockoutEnabled,
+                LockoutEnd = x.user.LockoutEnd,
+                NormalizedEmail = x.user.NormalizedEmail,
+                NormalizedUserName = x.user.NormalizedUserName,
+                PasswordHash = x.user.PasswordHash,
+                PhoneNumber = x.user.PhoneNumber,
+                UserName = x.user.UserName
+            }).ToList();
             return View();
         }
     }
